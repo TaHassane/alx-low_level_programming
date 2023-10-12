6ceb0e9dfbe377becbabb340fe0ea1e4b2a58085
TaHassane <taha.hassane777@gmail.com>
2023-10-12 16:04:44 -0700

    Task 3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,3 +9,15 @@
 
 void print_number(int n)
 {
+	unsigned int nb = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nb = -nb;
+	}
+	if ((nb / 10) > 0)
+		print_number(nb / 10);
+
+	_putchar((num % 10) + 48;
+}

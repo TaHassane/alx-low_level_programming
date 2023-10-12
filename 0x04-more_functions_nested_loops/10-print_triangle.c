708d47f3819db7bb0417d2513f93656acffbf1dd
TaHassane <taha.hassane777@gmail.com>
2023-10-12 14:38:44 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_triangle - prints a square, followed by a new line.
+* @size:  size of the triangle
+* Return: 0 (success)
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if ((i + j) <= size)
+				_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

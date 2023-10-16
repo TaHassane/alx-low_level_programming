6128ae93890012b771ef8a556ef92065c74d31fe
TaHassane <taha.hassane777@gmail.com>
2023-10-16 14:34:36 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* rev_string - prints a string, in reverse, followed by a new line.
+* @s: input 1
+* Return: 0 (success)
+*/
+void rev_string(char *s)
+{
+int l, i;
+char rev;
+
+for (l = 0; s[l] != '\0'; ++l)
+;
+
+for (i = 0; i < l / 2; i++)
+{
+rev = s[i];
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = rev;
+}
+}

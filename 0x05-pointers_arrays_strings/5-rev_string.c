0ac98bbfe0925ef3c2c514228b452730bd2e9eb0
TaHassane <taha.hassane777@gmail.com>
2023-10-16 14:35:56 -0700

    task 0

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,16 @@
 */
 void rev_string(char *s)
 {
-int l, i;
-char rev;
+	int l, i;
+	char rev;
 
-for (l = 0; s[l] != '\0'; ++l)
-;
+	for (l = 0; s[l] != '\0'; ++l)
+		;
 
-for (i = 0; i < l / 2; i++)
-{
-rev = s[i];
-s[i] = s[l - 1 - i];
-s[l - 1 - i] = rev;
-}
+	for (i = 0; i < l / 2; i++)
+	{
+		rev = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = rev;
+	}
 }

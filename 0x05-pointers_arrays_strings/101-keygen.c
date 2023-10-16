f2f145df059b4776d9e067c65129967c64bb31fd
TaHassane <taha.hassane777@gmail.com>
2023-10-16 16:01:44 -0700

    last commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdlio.h>
+
+/**
+* main - check code 
+* Return: 0 success
+*/
+int main(void)
+{
+
+	int sum;
+	char c;
+
+	strand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+
+	putchar(2772 - sum);
+
+	return (0);
+
+
+}

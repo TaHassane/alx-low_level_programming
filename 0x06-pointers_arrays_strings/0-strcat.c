6b54717f1c75113d6c6e0454dcaa57f9614bf15b
TaHassane <taha.hassane777@gmail.com>
2023-10-19 10:26:22 -0700

    task 0 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,17 @@
 char *_strcat(char *dest, char *src)
 {
 
-int i, j;
-i = 0;
-for (i = 0; dest[i] != '\0'; i++)
-;
+	int i, j;
 
-for (j = 0; src[j]; j++)
-dest[i++] = src[j++];
+	i = 0;
 
-return (dest);
+	while (dest[i])
+		i++;
+
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
+
+	return (dest);
 
 }
 

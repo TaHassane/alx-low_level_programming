c6da4d8023cc5452b6ce4fd85b8028edc44cd586
TaHassane <taha.hassane777@gmail.com>
2023-10-19 11:17:57 -0700

    task 0 fix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _strcmp - function that concatenates two strings
+* @a: input
+* @b: input
+* @n: the number of elements of the array
+* Return: no return
+*/
+void reverse_array(int *a, int n)
+{
+
+	int x, y, z;
+
+	for (x = 0, y = (n - 1); x < y; x++, y--)
+	{
+		z = a[x];
+		a[x] = a[y];
+		a[y] = z;
+	}
+
+
+}

de258045e7dcd5ee9c20da10c1211e50deb09783
TaHassane <taha.hassane777@gmail.com>
2023-10-19 11:42:41 -0700

    task 0 fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* *leet - function that encodes a string into 1337.
+* @c: output
+* Return: encoded string
+*/
+char *leet(char *s)
+{
+
+char *en = s;
+char c[] = {'A', 'E', 'O', 'T', 'L'}.
+int n[] = {4, 3, 0, 7, 1};
+unsigned int i;
+
+	while(*s)
+	{
+		for (i = 0; i < sizeof(c) / sizeof(char); i++)
+		{
+			if (*s == c[i] || *s == c[i] + 32)
+			{
+				*s = 48 + n[i];
+			}
+			c++;
+		}
+
+		return (en);
+}

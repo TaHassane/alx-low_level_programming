171504deccd42b02934ebb3fad6ba54b9b22f6d6
TaHassane <taha.hassane777@gmail.com>
2023-10-18 21:20:15 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdlio.h>
+
+/**
+* *_strcat - function that concatenates two strings
+* @dest: pointer to dest
+* @src: pointer to input
+* Return: string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+
+int i, j;
+i = 0;
+for (i = 0; dest[i] != 0; i++)
+;
+
+for (j = 0; src[j]; j++)
+dest[i++] = src[j++];
+
+return (dest);
+
+}
+

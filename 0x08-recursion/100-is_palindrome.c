590f981ddad5187060b065eed3f2ed036b78c78f
TaHassane <taha.hassane777@gmail.com>
2023-10-25 14:06:14 -0700

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* last - return the last position in a the string
+* @s: the string
+* Return: last position
+*/
+
+int pal(char *s, int deb, int fin, int x);
+int is_palindrome(char *s);
+int last(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n+= last(s + 1) + 1;
+	return (n);
+}
+
+/**
+* is_palindrome - that checks if the number is a palindrome
+* @s: the string
+* Return: returns 1 if a string is a palindrome and 0 if not
+*/
+
+int is_palindrome(char *s)
+{
+	return ((pal(s, 0, fin -1, fin % 2));
+}
+
+/**
+* pal - that checks if the number is a palindrome
+* @s: the string
+* @deb: cursor from left
+* @fin: cursor from right
+* @x: int
+* Return: returns 1 if a string is a palindrome and 0 if not
+*/
+int pal(char *s, int deb, int fin, int x)
+{
+	if ((deb == fin && x != 0) || (deb == fin + 1 && x == 0))
+		return (1);
+	else if (s[deb] != s[fin])
+		return(0);
+	return (check (s, deb +1, fin - 1, x));
+}

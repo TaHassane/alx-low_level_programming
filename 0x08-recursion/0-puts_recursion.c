b980138ff1a7467f114e9598ea6b4ea8ba7a6166
TaHassane <taha.hassane777@gmail.com>
2023-10-25 12:09:38 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
 /**
 * _puts_recursion -  function that prints a string
 * followed by a new line.
-* @s: string to input 
+* @s: string to input
 * Return: nothing to be returned
 */
 void _puts_recursion(char *s)

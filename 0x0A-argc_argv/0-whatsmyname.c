46e1424146ca588c5fc499ca610cf4ffd4dbd994
TaHassane <taha.hassane777@gmail.com>
2023-10-27 16:11:55 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - cheching between chars
+* @argc: int
+* @argv: array
+* Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+
+	(void) argc;
+	
+	printf("%s \n", argv[0]);
+	return (0);
+}
+

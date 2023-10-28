4fff58ccb559d04728f3e599a36dc387756587c2
TaHassane <taha.hassane777@gmail.com>
2023-10-27 18:55:42 -0700

    Taask 4wq

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,29 +2,39 @@
 #include <stdio.h>
 
 /**
-* main - sum of args
-* @argc: int
-* @argv: array
-* Return: 0 if arguments are given correcty
-*/
-int main(int argc, char const *argv[])
+ * main - sum of positive numbers
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ * Return: 0 if arguments are given correctly
+ */
+int main(int argc, char *argv[])
 {
 	int i, n = 0;
-	(void) argv;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return 0;
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		char *arg = argv[i];
+		int j = 0;
+
+		while (arg[j] != '\0')
 		{
-			return (printf("Error\n"), 1);
-		}
-		else
+			if (arg[j] < '0' || arg[j] > '9')
 		{
-			n += atoi(argv[i]);
+			printf("Error\n");
+			return 1;
 		}
+ 		j++;
+	}
+
+	n += atoi(arg);
 	}
 
 	printf("%d\n", n);
-return (0);
-
+	return 0;
 }

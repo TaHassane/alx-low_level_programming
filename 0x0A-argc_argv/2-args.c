fc77073505664989b819f1950cb912005facbd84
TaHassane <taha.hassane777@gmail.com>
2023-10-27 17:39:20 -0700

    Taask 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints the minimum number of coins to make
+* change for an amount of money.
+* @argc: int
+* @argv: array
+* Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+	for (i = 1; i < argc ; i++)
+		printf ("%s\n", argv[i]);
+return (0);
+
+}

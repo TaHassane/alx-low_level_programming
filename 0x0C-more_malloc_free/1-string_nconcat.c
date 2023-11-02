e0c777c1321bb16977b19f35f8cba2678708d713
TaHassane <taha.hassane777@gmail.com>
2023-11-02 16:49:27 -0700

    more malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,10 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s[i];
+		s[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
-		s[i] = s[j];
+		s[i] = s2[j];
 		i++;
 	}
 

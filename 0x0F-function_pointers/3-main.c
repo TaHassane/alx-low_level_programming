229d92904897953f7992017fe03981c64cad4081
TaHassane <taha.hassane777@gmail.com>
2023-11-08 17:47:46 -0800

    fucntion pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,7 +14,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int a, b;
-	char *op;
+	char *symb;
 
 	if (argc != 4)
 	{
@@ -23,22 +23,22 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	a = atoi(argv[1]);
-	op = argv[2];
+	symb = argv[2];
 	b = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(op) == NULL || symb[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	if ((*symb == '/' && b == 0) || (*symb == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(a, b));
+	printf("%d\n", get_op_func(symb)(a, b));
 
 	return (0);
 	}

cf536b17e03c6b88e8f941599c096917f9e3d127
TaHassane <taha.hassane777@gmail.com>
2023-11-20 16:22:12 -0800

    More singly linked lists commits

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * @head: head
+ *
+ * Return: if the linked list is empty return 0
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *x;
+	listint_t *h;
+	
+
+	if (*head == NULL)
+		return (0);
+
+	x = *head;
+
+	n = x->n;
+
+	h = x->next;
+
+	free(x);
+
+	*head = h;
+
+	return (n);
+}

d3dfddeb82d95bd31ccfc97db390ad765d08b8ef
TaHassane <taha.hassane777@gmail.com>
2023-11-20 16:37:20 -0800

    More singly linked lists commits

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * x_listint - function that returns the x of all the data (n)
+ * of a listint_t linked list.
+ * @head: if the list is empty, return 0
+ *
+ * Return: x of all the data (n).
+ */
+int x_listint(listint_t *head)
+{
+	int x;
+
+	x = 0;
+	while (head != NULL)
+	{
+		x += head->n;
+		head = head->next;
+	}
+
+	return (x);
+}

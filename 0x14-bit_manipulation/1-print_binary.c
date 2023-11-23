0d6a5fb33c7f4b985e2efd72804117f247fe8c8a
TaHassane <taha.hassane777@gmail.com>
2023-11-23 14:47:35 -0800

    Bit manipulation commits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints the binary representation 
+ * print_binary - function that prints the binary representation
  * of a number
  * @n: input of unsigned long int.
  *

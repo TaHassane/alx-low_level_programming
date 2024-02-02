a5796aaafcbdda6d8f048c193657ee4d55cbc223
TaHassane <m.taha.hassane@gmail.com>
2024-02-02 22:41:30 +0000

    Hash Tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+nclude "hash_tables.h"
+
+/**
+ * key_index - function that generates a distributed index
+ * according to a given hash
+ *
+ * @key: key
+ * @size: hash tables size
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+		return (0);
+	return (hash_djb2(key) % size);
+}

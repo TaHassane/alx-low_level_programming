06cf9840eee1f0ff28ca7b3d409b00a51420ad2c
TaHassane <m.taha.hassane@gmail.com>
2024-05-08 15:09:45 -0700

    Searching algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, unsigned int start, unsigned int end);
 #endif
